f8b31e861931a249b73b68932f996fce9147c807
katefranks <kmfranks9@gmail.com>
2021-07-31 17:33:15 -0400

    refactored app.js to integrate react router

--- frontend/react-pizza-app/src/components/App.js
+++ frontend/react-pizza-app/src/components/App.js
@@ -5,6 +5,11 @@ import MenuList from './MenuList';
 import Order from './Order';
 import ViewOrders from './ViewOrders';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav'
+import {BrowserRouter as Router,
+  Switch,
+  Route,
+  Link} from "react-router-dom";
 // import { Button , Modal } from 'react-bootstrap';
 
 class App extends Component {
@@ -66,50 +71,80 @@ class App extends Component {
     }
 
 
-  render() {
-    return (
-      <>
+//   render() {
+//     return (
+//       <>
+//
+//       <nav>
+//         <ul>
+//           <li onClick={() => this.setState({selection: 'menu'})}><p>Menu</p></li>
+//           <li onClick={() => this.setState({selection: 'kitchen'})}><p>Kitchen</p></li>
+//         </ul>
+//       </nav>
+//       <div className="main-menu">
+//         <header>
+//           <h1 className="main-header">Greenville Pizza Co.</h1>
+//           <p>411 University Ridge, Greenville, SC 29601</p>
+//           <p>555-555-555</p>
+//         </header>
+//
+//
+//
+//
+//         {
+//           this.state.selection === 'menu'
+//           ?
+//           <>
+//             <MenuList items={this.state.items} addToOrder={this.addToOrder}/>
+//             <Order order={this.state.order} removeItem={this.removeItem} saveOrder={this.saveOrder}/>
+//           </>
+//           :
+//           <ViewOrders order={this.state.order} />
+//         }
+//       </div>
+//       </>
+//   );}
+// }
+// export default App;
 
-      <nav>
-        <ul>
-          <li onClick={() => this.setState({selection: 'menu'})}><p>Menu</p></li>
-          <li onClick={() => this.setState({selection: 'kitchen'})}><p>Kitchen</p></li>
-        </ul>
-      </nav>
-      <div className="main-menu">
-        <header>
-          <h1 className="main-header">Greenville Pizza Co.</h1>
-          <p>411 University Ridge, Greenville, SC 29601</p>
-          <p>555-555-555</p>
-        </header>
-
-        {
-          this.state.selection === 'menu'
-          ?
+render() {
+  return (
+  <>
+  <Router>
+  <div className="main-menu">
+    <Nav variant="tabs" defaultActiveKey="/MenuList">
+      <Nav.Item>
+        <Nav.Link href="/MenuList">Menu</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/ViewOrders">Orders</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    <header>
+      <h1 className="main-header">Greenville Pizza Co.</h1>
+      <p>411 University Ridge, Greenville, SC 29601</p>
+      <p>555-555-555</p>
+    </header>
+    <Switch>
+      <Route
+        path='/MenuList'
+        render={(props) => (
           <>
-            <MenuList items={this.state.items} addToOrder={this.addToOrder}/>
-            <Order order={this.state.order} removeItem={this.removeItem} saveOrder={this.saveOrder}/>
+          <MenuList {...props} items={this.state.items} addToOrder={this.addToOrder} />
+          <Order order={this.state.order} removeItem={this.removeItem} saveOrder={this.saveOrder}/>
           </>
-          :
-          <ViewOrders order={this.state.order} />
-        }
-      </div>
-      </>
-  );}
+        )}
+        />
+      <Route
+        path='/ViewOrders'
+        render={(props) => (
+        <ViewOrders order={this.state.order} />
+        )}
+        />
+      </Switch>
+    </div>
+  </Router>
+  </>
+);}
 }
 export default App;
-
-// <Nav variant="tabs" defaultActiveKey="/home">
-//   <Nav.Item>
-//     <Nav.Link href="/home">Active</Nav.Link>
-//   </Nav.Item>
-//   <Nav.Item>
-//     <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-//   </Nav.Item>
-//   <Nav.Item>
-//     <Nav.Link eventKey="disabled" disabled>
-//       Disabled
-//     </Nav.Link>
-//   </Nav.Item>
-// </Nav>
-//
